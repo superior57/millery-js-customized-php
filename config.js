58c23ea63d8030d184269f5c0f2b9c0ff0e7a9e3
talentedexpert0057 <petpanda0057@gmail.com>
2021-06-01 04:10:00 +1000

    test update config.

--- config.js
+++ config.js
@@ -6,6 +6,8 @@ serverUri_arr.pop();
 const serverURL = serverUri_arr.join("/");
 const prefix = serverURL.replace(window.location.origin, "");
 
+console.log(serverURL, prefix);
+
 $('form').each(function() {
     this.action = prefix + this.action.replace(window.location.origin, "");
 })
